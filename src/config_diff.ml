d27731338c44a47dcf6e06d097a12f2dddd13bf6
John Estabrook <jestabro@vyos.io>
2022-02-03 18:23:05 -0600

    simplify and make tail recursive

--- src/config_diff.ml
+++ src/config_diff.ml
@@ -1,6 +1,4 @@
 type change = Unchanged | Added | Deleted | Updated of string list
-type config_diff_data = change * Config_tree.config_node_data
-type t = config_diff_data Vytree.t
 
 type diff_func = ?with_children:bool -> string list -> change -> unit
 
@@ -25,19 +23,6 @@ let name_of n = Vytree.name_of_node n
 let data_of n = Vytree.data_of_node n
 let children_of n = Vytree.children_of_node n
 let make data name children = Vytree.make_full data name children
-
-let modify_data (m : change) (a : Config_tree.config_node_data)
-                : config_diff_data = (m, a)
-let modify_node m = Vytree.fmap (modify_data m)
-
-let add_node (node : Config_tree.t) : t =
-    modify_node Added node
-let delete_node (node : Config_tree.t) : t =
-    modify_node Deleted node
-let keep_node (node : Config_tree.t) : t =
-    modify_node Unchanged node
-let update_node values (node : Config_tree.t) : t =
-    modify_node (Updated values) node
 
 let (^~) (node : Config_tree.t) (node' : Config_tree.t) =
   name_of node = name_of node' &&
@@ -78,25 +63,25 @@ let update_path path left_opt right_opt =
    resulting difference trees. The design of the scaffolding uses the idea
    of matching on pairs of (node opt) from
    https://github.com/LukeBurgessYeo/tree-diff
-   The construciton of the full scaffold is unnecessary for the diff_func
-   results, and is maintained for experiments and further anaylsis.
 *)
-let rec diff (path : string list) (f : diff_func) ((left_node_opt, right_node_opt) : Config_tree.t option * Config_tree.t option) : t =
-    let path = update_path path left_node_opt right_node_opt in
-    match left_node_opt, right_node_opt with
-    | Some left_node, None -> (f path Deleted; delete_node left_node)
-    | None, Some right_node -> (f path Added; add_node right_node)
-    | Some left_node, Some right_node when left_node = right_node ->
-            (f path Unchanged; keep_node left_node)
-    | Some left_node, Some right_node when left_node ^~ right_node ->
-            let values = (data_of right_node).values in
-            (f path (Updated values); update_node values left_node)
-    | Some left_node, Some right_node ->
-            (f ~with_children:false path Unchanged;
-            make (Unchanged, data_of left_node)
-                 (name_of left_node)
-                 (opt_zip left_node right_node |> List.map (diff path f)))
-    | None, None -> raise Empty_comparison
+let rec diff (path : string list) (f : diff_func) (l : (Config_tree.t option * Config_tree.t option) list) =
+    match l with
+    | [] -> ()
+    | (left_node_opt, right_node_opt) :: ls ->
+        (let path = update_path path left_node_opt right_node_opt in
+        match left_node_opt, right_node_opt with
+        | Some _, None -> f path Deleted
+        | None, Some _ -> f path Added
+        | Some left_node, Some right_node when left_node = right_node ->
+                f path Unchanged
+        | Some left_node, Some right_node when left_node ^~ right_node ->
+                let values = (data_of right_node).values in
+                f path (Updated values)
+        | Some left_node, Some right_node ->
+                (f ~with_children:false path Unchanged;
+                diff path f (opt_zip left_node right_node))
+        | None, None -> raise Empty_comparison)
+        ; diff path f ls
 
 (* copy node paths between trees *)
 let rec clone_path ?(with_children=true) old_root new_root path_done path_remaining =
@@ -144,10 +129,10 @@ let compare path left right =
         let (left, right) = if (List.length path > 0) then
             (tree_at_path path left, tree_at_path path right) else (left, right) in
         let trees = make_diff_tree left right in
-        let d = diff [] (decorate_trees trees) (Option.some left, Option.some right)
-        in (trees, d)
+        diff [] (decorate_trees trees) [(Option.some left, Option.some right)];
+        trees
 
 (* wrapper to return diff trees *)
 let diffs path left right =
-    let trees, _ = compare path left right in
+    let trees = compare path left right in
     (!(trees.add), !(trees.del), !(trees.inter))
--- src/config_diff.mli
+++ src/config_diff.mli
@@ -1,6 +1,4 @@
 type change = Unchanged | Added | Deleted | Updated of string list
-type config_diff_data = change * Config_tree.config_node_data
-type t = config_diff_data Vytree.t
 
 type diff_func = ?with_children:bool -> string list -> change -> unit
 
@@ -18,6 +16,6 @@ exception Empty_comparison
 val make_diff_tree : Config_tree.t -> Config_tree.t -> diff_trees
 val clone : ?with_children:bool -> 'a Vytree.t -> 'a Vytree.t -> string list -> 'a Vytree.t
 val decorate_trees : diff_trees -> ?with_children:bool -> string list -> change -> unit
-val compare : string list -> Config_tree.t -> Config_tree.t -> diff_trees * t
+val compare : string list -> Config_tree.t -> Config_tree.t -> diff_trees
 val diffs : string list -> Config_tree.t -> Config_tree.t -> Config_tree.t * Config_tree.t * Config_tree.t
 
