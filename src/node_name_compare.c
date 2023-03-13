b4a46d1d4ae4bde712eea108abcebec1855ad90e
John Estabrook <jestabro@vyos.io>
2023-03-13 12:03:21 -0500

    use OCaml values

--- src/node_name_compare.c
+++ src/node_name_compare.c
@@ -1,16 +1,29 @@
-
-#include <stdlib.h>
+/*
+ * numeric-lexicographical compare
+ */
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
 
-int node_name_compare(const char* s1, const char* s2) {
-    const char *p1 = s1;
-    const char *p2 = s2;
-    int pos = 0;
-    int len1 = strlen(s1);
-    int len2 = strlen(s2);
-    int len = len1 <= len2 ? len1 : len2;
+CAMLprim value caml_node_name_compare(value str1, value str2) {
+    mlsize_t len, len1, len2;
+    int pos, pos1, pos2;
+    const char * s1, * s2;
+    const char * p1, * p2;
+    int n1, n2;
+    int res;
+
+    if (str1 == str2) return Val_int(0);
+    s1 = String_val(str1);
+    s2 = String_val(str2);
+    len1 = strlen(s1);
+    len2 = strlen(s2);
+    len = len1 <= len2 ? len1 : len2;
+    p1 = s1;
+    p2 = s2;
+    pos = 0;
 
     do {
         while ((pos < len) && (!isdigit(*s1) || !isdigit(*s2))) {
@@ -18,18 +31,14 @@ int node_name_compare(const char* s1, const char* s2) {
             s2++;
             pos++;
         }
-        if (pos) {
-            int r = memcmp(p1, p2, pos);
-            if (r) {
-                int res = r < 1 ? -1 : 1;
-                return res;
-            }
-            else {
-                if (pos == len) {
-                    if (len1 < len2) return -1;
-                    if (len2 < len1) return 1;
-                    return 0;
-                }
+        if (pos > 0) {
+            res = memcmp(p1, p2, pos);
+            if (res < 0) return Val_int(-1);
+            if (res > 0) return Val_int(1);
+            if (pos == len) {
+                if (len1 < len2) return Val_int(-1);
+                if (len2 < len1) return Val_int(1);
+                return Val_int(0);
             }
         }
         p1 = s1;
@@ -37,11 +46,8 @@ int node_name_compare(const char* s1, const char* s2) {
         len = len - pos;
         len1 = len1 - pos;
         len2 = len2 - pos;
-        pos = 0;
-        int pos1 = pos;
-        int pos2 = pos;
-        int n1 = 0;
-        int n2 = 0;
+        pos1 = pos2 = 0;
+        n1 = n2 = 0;
         while ((pos1 < len1) && isdigit(*s1)) {
             n1 = n1 * 10 + *s1 - '0';
             s1++;
@@ -52,28 +58,16 @@ int node_name_compare(const char* s1, const char* s2) {
             s2++;
             pos2++;
         }
-        if (n1 < n2) return -1;
-        if (n2 < n1) return 1;
+        if (n1 < n2) return Val_int(-1);
+        if (n2 < n1) return Val_int(1);
         if ((pos1 == len1) || (pos2 == len2)) {
-            if (len1 < len2) return -1;
-            if (len2 < len1) return 1;
-            return 0;
+            if (len1 < len2) return Val_int(-1);
+            if (len2 < len1) return Val_int(1);
+            return Val_int(0);
 
         }
+        // if here, pos1 == pos2
+        pos = pos1;
+        len = len - pos;
     } while (*s1 && *s2);
-}
-
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Requires two strings to compare\n");
-        exit(-1);
-    }
-
-    char *str1 = strdup(argv[1]);
-    char *str2 = strdup(argv[2]);
-
-    int res = node_name_compare(str1, str2);
-
-    printf("result: %d\n", res);
-
 }
