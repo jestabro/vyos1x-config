dbc47ae9363b2e60b10a70a2297f4fdfb87f1cae
John Estabrook <jestabro@vyos.io>
2023-08-21 14:23:45 -0500

    T5500: remove empty braces after valueless node
    (cherry picked from commit 9f842e6dcf83095f463fe7b26c575bc85f62baca)

--- src/config_tree.ml
+++ src/config_tree.ml
@@ -161,7 +161,7 @@ struct
 
   let render_values indent_str name values =
     match values with
-    | [] -> Printf.sprintf "%s%s { }\n" indent_str name
+    | [] -> Printf.sprintf "%s%s\n" indent_str name
     | [v] -> Printf.sprintf "%s%s \"%s\"\n" indent_str name (Util.escape_string v)
     | _  -> 
       let rendered = List.map (fun s -> Printf.sprintf "%s%s \"%s\"" indent_str name (Util.escape_string s)) values in
