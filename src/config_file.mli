0789f68d6f8fa388cd09bf9a88e10488c719f92e
John Estabrook <jestabro@vyos.io>
2025-12-02 19:19:24 -0600

    crude test

--- src/config_file.mli
+++ src/config_file.mli
@@ -1,4 +1,8 @@
 
+val escape_backslash : string -> string
+
+val unescape_backslash : string -> string
+
 val load_config : string -> (Config_tree.t, string) result
 
 val save_config : Config_tree.t -> string -> (unit, string) result
