91b41f9bce1c25e061e6025669974b964710296b
John Estabrook <jestabro@vyos.io>
2026-01-20 20:38:40 -0600

    clean up naming

--- src/config_dict.ml
+++ src/config_dict.ml
@@ -3,13 +3,15 @@
 
 (*let config_dict ?(mangle=false) ?(no_tag_mangle=false)*)
 
-let hybrid_tree ?(with_first_node=true) rt ct mask path =
-    let ct_sub = Config_tree.get_subtree ~with_node:with_first_node ct path in
-    let add_defaults ct' p' =
-        let rpath = Reference_tree.refpath rt (path @ p') in
-        let rt_sub = Reference_tree.get_subtree rt rpath in
-        let res =
-            let fold_defaults (p, (continue, acc)) node =
+let hybrid_tree ?(with_first_node=true) ref_tree config_tree mask path =
+    let ct_at_path =
+        Config_tree.get_subtree ~with_node:with_first_node config_tree path
+    in
+    let add_defaults ct p' =
+        let ref_path = Reference_tree.refpath ref_tree (path @ p') in
+        let relative_ref_tree = Reference_tree.get_subtree ref_tree ref_path in
+        let result =
+            let ref_tree_walk (p, (continue, acc)) node =
                 if not continue
                 then (p, (false, acc))
                 else
@@ -19,7 +21,7 @@ let hybrid_tree ?(with_first_node=true) rt ct mask path =
                 then (p, (continue, acc))
                 else
                 if (Vytree.is_terminal_path[@alert "-exn"]) mask total_path &&
-                   (Vytree.exists[@alert "-exn"]) ct' total_path
+                   (Vytree.exists[@alert "-exn"]) ct total_path
                 then (p, (false, acc))
                 else
                 let data = Vytree.data_of_node node in
@@ -41,21 +43,21 @@ let hybrid_tree ?(with_first_node=true) rt ct mask path =
                             in (p, (continue, acc'))
                     end
                 | _ -> (p, (continue, acc))
-            in Vytree.fold_tree_with_path fold_defaults ([], (true, ct')) rt_sub
-        in snd res
+            in Vytree.fold_tree_with_path ref_tree_walk ([], (true, ct)) relative_ref_tree
+        in snd result
     in
-    let ct_func (p, acc) _c =
+    let config_tree_walk (p, acc) _ct =
         let rev_p = List.rev p in
-        let c' = add_defaults acc rev_p
-        in (p, c')
+        let ct' = add_defaults acc rev_p
+        in (p, ct')
         (*
         if Vytree.is_terminal_path c p then
             let c' = add_defaults acc p
             in (p, c')
         else (p, acc)
         *)
-    in Vytree.fold_tree_with_path ct_func ([], ct_sub) ct_sub
+    in Vytree.fold_tree_with_path config_tree_walk ([], ct_at_path) ct_at_path
 
 
-let config_dict _rt ct =
+let config_dict _ref_tree ct =
     Config_tree.render_json ct
